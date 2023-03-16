79ce8730a86fc3ad33504e5543851c295a98a25b
Diego Cardoso <diegocar448@hotmail.com>
2023-03-16 10:26:03 -0300

    Injeção de dependencia mock BD

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,21 +1,35 @@
+export interface User {
+    name:string
+    email:string
+}
+
+
 const db = [
     {
-        "name": "Reuven",
-        "email": "reuven@didi.com"
+        name: "Reuven",
+        email: "reuven@didi.com",
     }
 ]
 
 export class UserService{
+    db: User[]
+
+    constructor(
+        database = db
+    ){
+        this.db = database
+    }
+
     createUser = (name:string, email:string) => {
         const user = {
             name,
             email
         }
-        db.push(user)
-        console.log("DB atualizado", db)
+        this.db.push(user)
+        console.log("DB atualizado", this.db)
     }
 
     getAllUsers = () => {
-        return db;
+        return this.db;
     }
 }
