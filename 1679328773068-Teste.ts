92aa1f05b68774782b594a6a758c71a5e7353e3b
Diego Cardoso <diegocar448@hotmail.com>
2023-03-20 13:46:22 -0300

    Criando arquivo ormconfig.js e setar as configurações de script no packages.json

---
+++ 1679328773068-Teste.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class Teste1679328773068 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
